3ecf9eb33c894ec4328fe615a5ae71bdd880e91e
Soner Coşkun <sonercoskun95@gmail.com>
2022-10-01 18:34:04 +0300

    initial

---
+++ src/main/java/com/company/DataType.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public enum DataType {
+
+    TWO_G("2G"),
+    FOUR_G("4G"),
+    LATE_NIGHT("LATE_NIGHT"),
+    OFF_PEAK("OFF_PEAK");
+
+
+    private final String value;
+
+    DataType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static DataType fromString(String str) {
+        for (DataType dataType : values()) {
+            if (dataType.getValue().equalsIgnoreCase(str)) {
+                return dataType;
+            }
+        }
+        return null;
+    }
+
+
+}
